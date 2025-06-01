ca9bc3e7a6f1f149bd4e92c85a208054237b3472
PeterLee422 <81627774+PeterLee422@users.noreply.github.com>
2025-06-01 16:07:55 +0900

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -12,7 +12,7 @@ from app.models.friend import FriendRequest, Friend
 
 DATABASE_URL = (
     f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}"
-    f"@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+    f"@{settings.POSTGRES_HOST}.singapore-postgres.render.com:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
 )
 
 
